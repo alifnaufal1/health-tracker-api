40e4945081e5592594026ee96e02f0f9d994e16c
alif_naufal <naufalmenjangan@gmail.com>
2026-09-13 17:00:34 +0700

    fix: make workout batch data as json

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module health-tracker-api
 go 1.25.3
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.2.2 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/gofiber/contrib/v3/jwt v1.2.1 // indirect
 	github.com/gofiber/fiber/v3 v3.5.0 // indirect
@@ -46,6 +48,8 @@ require (
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
+	gorm.io/datatypes v1.2.7 // indirect
+	gorm.io/driver/mysql v1.5.6 // indirect
 	gorm.io/driver/postgres v1.6.2 // indirect
 	gorm.io/gorm v1.31.2 // indirect
 )
--- internal/model/domain/workout_data.go
+++ internal/model/domain/workout_data.go
@@ -4,26 +4,27 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
 )
 
 type WorkoutData struct {
 	Base
-	WorkoutDataType  string `gorm:"not null"`
-	TotalSteps       int    `gorm:"not null"`
-	TotalDistance    int    `gorm:"not null"`
-	TotalCalory      int    `gorm:"not null"`
-	HeartRateAverage int    `gorm:"not null"`
-	Pace             int    `gorm:"not null"`
-	WorkoutBatchData []WorkoutBatchData
+	WorkoutDataType  string                                `gorm:"not null"`
+	TotalSteps       int                                   `gorm:"not null"`
+	TotalDistance    int                                   `gorm:"not null"`
+	TotalCalory      int                                   `gorm:"not null"`
+	HeartRateAverage int                                   `gorm:"not null"`
+	Pace             int                                   `gorm:"not null"`
+	WorkoutBatchData datatypes.JSONSlice[WorkoutBatchData] `gorm:"type:jsonb;not null"`
 	DeviceId         *uuid.UUID
-	Device 			 Device
+	Device           Device
 }
 
 type WorkoutBatchData struct {
-	TotalSteps       int    
-	TotalDistance    int    
-	TotalCalories      int    
-	HeartRateAverage int    
-	Pace             int    
-	CreatedAt		 time.Time
+	TotalSteps       int       `json:"total_steps"`
+	TotalDistance    int       `json:"total_distance"`
+	TotalCalories    int       `json:"total_calories"`
+	HeartRateAverage int       `json:"heart_rate_average"`
+	Pace             int       `json:"pace"`
+	CreatedAt        time.Time `json:"created_at"`
 }
